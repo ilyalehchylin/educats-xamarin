2341db0aa93f628cfef5b0ac3402f56c98fa4d6e
Ilya Lehchylin <i.lehchylin@nyxbull.com>
2020-02-15 11:45:42 +0300

    [ADD] Initial project

---
+++ src/EduCATS/App.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace EduCATS
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			MainPage = new ContentPage {
+				Content = new Label {
+					Text = "EduCATS"
+				}
+			};
+		}
+	}
+}
