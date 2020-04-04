05eafe8d25e0226183f274da5053bdfea8b974ac
Ilya Lehchylin <lehchylin.ilya@gmail.com>
2020-04-04 14:30:51 +0300

    [ADD] Json controller & json object controller tests

--- source/EduCATS.UnitTests/JsonControllerTests.cs
+++ source/EduCATS.UnitTests/JsonControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using EduCATS.Helpers.Json;
+using EduCATS.Networking.Models.Login;
+using Moq;
 using NUnit.Framework;
 
 namespace EduCATS.UnitTests
@@ -10,6 +12,11 @@ namespace EduCATS.UnitTests
 		const string nonValidJson = "Not JSON";
 		const string validJson = "{ \"data\": 25 }";
 		const string validJsonArray = "[ { \"data\": 25 }, { \"data\": 50 } ]";
+
+		const string username = "admin";
+		const string password = "123";
+
+		static string userJson = $"{{ \"username\": \"{username}\", \"password\": \"{password}\" }}";
 
 		[Test]
 		public void IsSingleJsonValidTest()
@@ -26,9 +33,16 @@ namespace EduCATS.UnitTests
 		}
 
 		[Test]
-		public void IsJsonValidNegativeTest()
+		public void IsNonJsonValidTest()
 		{
 			var actual = JsonController.IsJsonValid(nonValidJson);
+			Assert.AreEqual(false, actual);
+		}
+
+		[Test]
+		public void IsNullJsonValid()
+		{
+			var actual = JsonController.IsJsonValid(null);
 			Assert.AreEqual(false, actual);
 		}
 
@@ -45,6 +59,31 @@ namespace EduCATS.UnitTests
 		{
 			var actual = JsonController.ConvertObjectToJson(null);
 			Assert.AreEqual(null, actual);
+		}
+
+		[Test]
+		public void ConvertJsonToObjectTest()
+		{
+			var user = Mock.Of<UserCredentials>(
+				u => u.Username == "admin" && u.Password == "123");
+
+			var actual = JsonController<UserCredentials>.ConvertJsonToObject(userJson);
+			Assert.AreEqual(user.Username, actual.Username);
+			Assert.AreEqual(user.Password, actual.Password);
+		}
+
+		[Test]
+		public void ConvertNullJsonToObjectTest()
+		{
+			var actual = JsonController<object>.ConvertJsonToObject(null);
+			Assert.AreEqual(default, actual);
+		}
+
+		[Test]
+		public void ConvertEmptyJsonToObjectTest()
+		{
+			var actual = JsonController<object>.ConvertJsonToObject("");
+			Assert.AreEqual(default, actual);
 		}
 	}
 }
