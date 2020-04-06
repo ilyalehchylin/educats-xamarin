d53cadd60da1b39a98583b0d4b0fe7fdc50d8de6
Ilya Lehchylin <lehchylin.ilya@gmail.com>
2020-04-06 23:33:02 +0300

    [ADD] Unit tests for Helpers

---
+++ source/EduCATS.UnitTests/DateTimeExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using EduCATS.Helpers.Date.Extensions;
+
+namespace EduCATS.UnitTests
+{
+	[TestFixture]
+	public class DateTimeExtensionsTests
+	{
+		[Test]
+		public void StartOfWeekTest()
+		{
+			var date = new DateTime(2020, 4, 10);
+			var actual = date.StartOfWeek(DayOfWeek.Monday);
+			var expected = new DateTime(2020, 4, 6);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
