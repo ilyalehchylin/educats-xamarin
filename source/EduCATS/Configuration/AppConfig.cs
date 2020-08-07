4b25a40046db9d133916072f35bbef13f06c8276
Ilya Lehchylin <lehchylin.ilya@gmail.com>
2020-08-07 10:47:50 +0300

    [ADD] New languages: German, Polish

--- source/EduCATS/Configuration/AppConfig.cs
+++ source/EduCATS/Configuration/AppConfig.cs
@@ -82,6 +82,8 @@ namespace EduCATS.Configuration
 			CrossLocalization.AddLanguageSupport(Languages.RU);
 			CrossLocalization.AddLanguageSupport(Languages.LT);
 			CrossLocalization.AddLanguageSupport(Languages.BE);
+			CrossLocalization.AddLanguageSupport(Languages.DE);
+			CrossLocalization.AddLanguageSupport(Languages.PL);
 			CrossLocalization.SetDefaultLanguage(Languages.EN.LangCode);
 			CrossLocalization.SetLanguage(_services.Preferences.LanguageCode);
 		}
