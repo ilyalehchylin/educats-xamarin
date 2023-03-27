2b9bb2c390061c8ed8336ad27b49e6ef76b5b264
ваня <@DESKTOP-BLDCGMT>
2023-03-27 20:23:50 +0300

    [FIX] Edit models

--- source/EduCATS/Pages/Statistics/Results/Models/StatsPageLabsRatingModel.cs
+++ source/EduCATS/Pages/Statistics/Results/Models/StatsPageLabsRatingModel.cs
@@ -5,16 +5,18 @@ namespace EduCATS.Pages.Statistics.Results.Models
 {
 	public class StatsPageLabsRatingModel : IRoundedListType
 	{
-		public StatsPageLabsRatingModel(int labId, string shortName, string theme)
+		public StatsPageLabsRatingModel(int labId, string shortName, string theme, int subGroup)
 		{
 			LabId = labId;
 			ShortName = shortName;
 			Theme = theme;
+			SubGroup = subGroup;
 		}
 
 		public int LabId { get; set; }
 		public string ShortName { get; set; }
 		public string Theme { get; set; }
+		public int SubGroup { get; set; }
 
 		public RoundedListTypeEnum GetListType()
 		{
